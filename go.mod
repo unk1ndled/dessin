c6ac950bcf208ac673e6ab95731ca16b59f5cb2b
Amine Salmane <amine.games2002@gmail.com>
2024-06-30 14:11:54 +0100

    window setup

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/unk1ndled/draw
+
+go 1.21.4
+
+require github.com/veandco/go-sdl2 v0.4.40
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/unk1ndled/draw/sdl"
+)
+
+func main() {
+
+	sdl.Visualise("test", 400, 400)
+}
